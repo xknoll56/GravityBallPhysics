e1b24d2ebf66471a697812fb37d47dd8b0506418
Xavier Knoll <xavierknoll56@gmail.com>
2026-06-27 11:28:54 -0700

    Squashed commit of the following:
    commit 7b978a27fc263103fc085f8c4c6a0052ce525f08
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Sat Jun 27 11:28:26 2026 -0700
    
        Update GBSimulation.h
    
    commit 9b17001d1f612b47f5f8753788a4264affbe41a3
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Sat Jun 27 11:22:51 2026 -0700
    
        fix multibody
    
    commit fdf66d3e5e1ddd67ade0f927d401391b1774683b
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Sat Jun 27 11:04:58 2026 -0700
    
        multibody sample
    
    commit c835f4fc768f1c7ba0d64534d99f841670649cc9
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Sat Jun 27 09:06:49 2026 -0700
    
        Update BoxStack.umap
    
    commit 91ed0a3028cb2e788b8490b49e0a638bc7d93a4e
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Sat Jun 27 09:02:59 2026 -0700
    
        modify lighting in scene
    
    commit be79fd83e3aaca890b266a3540e8e98d83d3a1a1
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Sat Jun 27 08:37:36 2026 -0700
    
        adjust scene/grid size
    
    commit 2e26ce550f8f039d7a1e8774e9226b0bc27a8459
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Fri Jun 26 23:57:47 2026 -0700
    
        revert some scene stuff
    
    commit 8aee4662060e8f10870818a68c2ac1e68590b960
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Fri Jun 26 19:29:27 2026 -0700
    
        fix bug
    
    commit ae697966e82bbabb199354613a1e8a962056c6b2
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Fri Jun 26 14:56:50 2026 -0700
    
        physics scene testing
    
    commit a40ef137ac6b9dc0d517c18de1136d387787e8d8
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Fri Jun 26 13:19:39 2026 -0700
    
        Update Default.umap
    
    commit 13648f53a800ce3f086d570872e65f5e9e1e3df5
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Fri Jun 26 13:17:11 2026 -0700
    
        basic default scene
    
    commit d4a242c553610f9fc7cd3e12c9f9a9fc0e1bf4e2
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Thu Jun 25 23:02:20 2026 -0700
    
        create unreal demo project
    
    commit 2a02a58c547b7c11896163fc6d80ceca62237322
    Author: Xavier Knoll <xavierknoll56@gmail.com>
    Date:   Thu Jun 25 21:29:47 2026 -0700
    
        make inline functions

--- GravityBallUnreal/Source/GravityBallUnreal/PGBWorld.cpp
+++ GravityBallUnreal/Source/GravityBallUnreal/PGBWorld.cpp
@@ -75,18 +75,191 @@ void APGBWorld::initSceneBoxStack()
 		shootableBody->transform.position = { -25.0f,-10.0f + 2.0f * i,0.5f };
 		GBSphereCollider* pSphere = simulation.attachSphereCollider(shootableBody, 0.5f + (i * 0.1f) * 0.75);
 		pSphere->pData = new RenderableCollider({ 0.1f * (i % 2),0.76f * (i + 1 % 2), 0.92f });
-		shootableBody->setMass(5.0f * pBox->volume());
+		shootableBody->setMass(5.0f * pSphere->volume());
 		shootStack[i] = shootableBody;
 	}
 
 	shootableBody = shootStack[0];
 	shootCount = 10;
 
-	pBody = simulation.createBody();
-	pBox = simulation.attachBoxCollider(pBody, { 100.0f,100.0f,0.1f });
-	pBody->transform.position = { 0.0f,0.0f,-0.05f };
-	pBody->isStatic = true;
 	sceneEnum = SCENE_BOX;
+}
+
+void APGBWorld::initSceneMultibody()
+{
+
+	// --- G ---
+	{
+		GBBody* C = simulation.createBody();
+
+		float thickness = 0.25f;
+		float radius = 1.0f;
+		GBVector3 color = { 0,0.6,1 };
+		for (int i = 0; i < 5; i++)
+		{
+			float portion = i / 5.0f;
+			float nextPotion = ((i + 1) % 5) / 5.0f;
+			const float heightModifier = 1.5f;
+			
+			if (i < 4)
+			{
+				GBVector3 p1 = GBVector3(0, radius * cos(portion * 2 * GB_PI), radius * heightModifier * sin(portion * 2 * GB_PI));
+				GBVector3 p2 = GBVector3(0, radius * cos(nextPotion * 2 * GB_PI), radius * heightModifier * sin(nextPotion * 2 * GB_PI));
+				GBCapsuleCollider testCap = GBCapsuleCollider::capsuleFromEdge({ p1, p2 });
+				GBCapsuleCollider* pCap = simulation.attachCapsuleCollider(C, thickness, testCap.height,
+					GBTransform(testCap.transform.position - C->transform.position, testCap.transform.rotation));
+				setRenderableData(pCap, color);
+			}
+			else
+			{
+				GBVector3 p1 = GBVector3(0, radius * cos(portion * 2 * GB_PI), radius * heightModifier * sin(portion * 2 * GB_PI));
+				GBVector3 p2 = p1 + GBVector3(0, 0, 1.0f);
+				GBCapsuleCollider testCap = GBCapsuleCollider::capsuleFromEdge({ p1, p2 });
+				GBCapsuleCollider* pCap = simulation.attachCapsuleCollider(C, thickness, testCap.height,
+					GBTransform(testCap.transform.position - C->transform.position, testCap.transform.rotation));
+				setRenderableData(pCap, color);
+				p1 = p2;
+				p2 = p1 + GBVector3(0, -0.5, 0.0f);
+
+				testCap = GBCapsuleCollider::capsuleFromEdge({ p1, p2 });
+				pCap = simulation.attachCapsuleCollider(C, thickness, testCap.height,
+					GBTransform(testCap.transform.position - C->transform.position, testCap.transform.rotation));
+				setRenderableData(pCap, color);
+			}
+
+		}
+
+		GBVector3 min = C->aabb.low();
+		setRenderableData(simulation.attachBoxCollider(C, { 0.25f, C->aabb.halfExtents.y,0.1f }, GBTransform(-C->transform.position
+			+ min.zComponent() * (1.05f) + GBVector3::right() * 0.25f)), color);
+		setRenderableData(simulation.attachBoxCollider(C, {C->aabb.halfExtents.y, 0.25f,0.1f }, GBTransform(-C->transform.position
+			+ min.zComponent() * (1.05f) + GBVector3::right() * 0.25f)), color);
+
+		C->transform.position = { 0, -2, 2 };
+	}
+
+	// --- B ---
+	{
+		GBBody* B = simulation.createBody();
+
+		float thickness = 0.25f;
+		float radius = 0.80f;
+
+		GBVector3 firstPoint, lastPoint;
+		GBVector3 color = { 0.0f, 1.0f, 0.25f };
+		for (int i = 0; i < 5; i++)
+		{
+			float portion = i / 6.0f;
+			float nextPotion = ((i + 1) % 6) / 6.0f;
+			const float widthModifier = 1.5f;
+			GBVector3 p1 = GBVector3(0, radius * widthModifier * sin(portion * GB_PI * 1.25), radius * -cos(portion * GB_PI * 1.25));
+			GBVector3 p2 = GBVector3(0, radius * widthModifier * sin(nextPotion * GB_PI * 1.25), radius * -cos(nextPotion * GB_PI * 1.25));
+			GBCapsuleCollider testCap = GBCapsuleCollider::capsuleFromEdge({ p1, p2 });
+			GBCapsuleCollider* pCap = simulation.attachCapsuleCollider(B, thickness, testCap.height,
+				GBTransform(testCap.transform.position - B->transform.position, testCap.transform.rotation));
+			setRenderableData(pCap, color);
+			if (i == 4)
+				lastPoint = p2;
+
+		}
+		for (int i = 0; i < 5; i++)
+		{
+			float portion = i / 6.0f;
+			float nextPotion = ((i + 1) % 6) / 6.0f;
+			const float widthModifier = 1.5f;
+			GBVector3 offset = { 0,0,-2.0f * radius };
+			GBVector3 p1 = offset + GBVector3(0, radius * widthModifier * sin(portion * GB_PI * 1.25), radius * -cos(portion * GB_PI * 1.25));
+			GBVector3 p2 = offset + GBVector3(0, radius * widthModifier * sin(nextPotion * GB_PI * 1.25), radius * -cos(nextPotion * GB_PI * 1.25));
+			GBCapsuleCollider testCap = GBCapsuleCollider::capsuleFromEdge({ p1, p2 });
+			GBCapsuleCollider* pCap = simulation.attachCapsuleCollider(B, thickness, testCap.height,
+				GBTransform(testCap.transform.position - B->transform.position, testCap.transform.rotation));
+			setRenderableData(pCap, color);
+			if (i == 0)
+				firstPoint = p1;
+			else if (i == 4)
+			{
+				p1 = firstPoint;
+				p2 = lastPoint;
+				testCap = GBCapsuleCollider::capsuleFromEdge({ p1, p2 });
+				pCap = simulation.attachCapsuleCollider(B, thickness, testCap.height,
+					GBTransform(testCap.transform.position - B->transform.position, testCap.transform.rotation));
+				setRenderableData(pCap, color);
+			}
+		}
+		
+		GBVector3 min = B->aabb.low();
+		setRenderableData(simulation.attachBoxCollider(B, { 0.25f, B->aabb.halfExtents.y,0.1f }, GBTransform(-B->transform.position
+			+ min.zComponent()*(1.05f) + GBVector3::right() * 0.25f)), color);
+		setRenderableData(simulation.attachBoxCollider(B, { B->aabb.halfExtents.y, 0.25f,0.1f }, GBTransform(-B->transform.position
+			+ min.zComponent() * (1.05f) + GBVector3::right() * 0.25f)), color);
+
+		B->transform.position = { 0, 2, 2 };
+	}
+
+	// Box
+	{
+		for (int start = 0; start < 4; start++)
+		{
+			GBVector3 startPos;
+			GBVector3 color;
+			switch (start)
+			{
+			case 0:
+				startPos = { 5, 5, 5 };
+				color = { 1,0,0 };
+				break;
+			case 1:
+				startPos = { -5, 5, 5 };
+				color = { 0,1,0 };
+				break;
+			case 2:
+				startPos = { -5, -5, 5 };
+				color = { 0,0,1 };
+				break;
+			case 3:
+				startPos = { 5, -5, 5 };
+				color = { 0.74,0.83, 0.94 };
+				break;
+			}
+			GBBody* Box = simulation.createBody();
+			GBEdge edges[4];
+			GBBoxCollider bc({ 1.0,1.0,1.0 });
+			for (int i = 0; i < 6; i++)
+			{
+				GBCardinal faceDir = (GBCardinal)i;
+				GBQuad face = GBManifoldGeneration::GBBoxCardinalToQuad(bc, faceDir);
+				face.extractEdges(edges);
+				for (int j = 0; j < 4; j++)
+				{
+					if ((i == 4 || i == 5) || GBAbs(edges[j].a.z - edges[j].b.z) > 0.0f)
+					{
+						GBCapsuleCollider testCap = GBCapsuleCollider::capsuleFromEdge({ edges[j].a, edges[j].b });
+						GBCapsuleCollider* pCap = simulation.attachCapsuleCollider(Box, 0.2f, testCap.height,
+							GBTransform(testCap.transform.position - Box->transform.position, testCap.transform.rotation));
+						setRenderableData(pCap, color);
+					}
+				}
+			}
+			simulation.recenterMass(Box);
+			Box->transform.position = startPos;
+		}
+	}
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		shootableBody = simulation.createBody();
+		shootableBody->transform.position = { -25.0f,-10.0f + 2.0f * i,0.5f };
+		GBSphereCollider* pSphere = simulation.attachSphereCollider(shootableBody, 0.5f + (i * 0.1f) * 0.75);
+		pSphere->pData = new RenderableCollider({ 0.1f * (i % 2),0.76f * (i + 1 % 2), 0.92f });
+		shootableBody->setMass(5.0f * pSphere->volume());
+		shootStack[i] = shootableBody;
+	}
+
+	shootableBody = shootStack[0];
+	shootCount = 10;
+
+	sceneEnum = SCENE_MULTIBODY;
 }
 
 // Called when the game starts or when spawned
@@ -101,6 +274,10 @@ void APGBWorld::BeginPlay()
 	if (levelName == "BoxStack")
 	{
 		initSceneBoxStack();
+	}
+	else if (levelName == "Multibody")
+	{
+		initSceneMultibody();
 	}
 
 
